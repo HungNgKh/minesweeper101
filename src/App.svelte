670346c3909a60357f73533db7cfe6299eccabd9
Kain <khanhhung.nguyen.dev@gmail.com>
2024-08-11 21:37:06 +0900

    🔥 Delete component:w

--- src/App.svelte
+++ src/App.svelte
@@ -15,10 +15,6 @@
   </div>
   <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
-
   <p>
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
   </p>
